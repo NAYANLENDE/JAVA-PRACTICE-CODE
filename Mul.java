c1f83a00eba15eb2fb5e84d375141bc124b3b59e
Nayan Lende <127320935+NAYANLENDE@users.noreply.github.com>
2023-03-09 16:22:50 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,12 @@
+
+class Mul {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=98;
+		int res=a*b;
+		System.out.println("Result is "+res);
+		
+	}
+
+}
---
+++ Sub.java
@@ -0,0 +1,12 @@
+
+ class Sub {
+
+	public static void main(String[] args) {
+		int a=56;
+		int b=45;
+		int res=a-b;
+		System.out.println("Result is "+res);
+
+	}
+
+}
